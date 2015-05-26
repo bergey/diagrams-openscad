0b4219679bcdb826f1a9f747d48e0cd9da7202d9
Daniel Bergey <bergey@alum.mit.edu>
2015-05-26 18:53:37 +0000

    add a trivial example

---
+++ examples/Solids.hs
@@ -0,0 +1,13 @@
+-- | A very simple test of diagrams-openscad
+
+module Main where
+
+import Diagrams.Prelude
+
+import Diagrams.Backend.OpenSCad
+import Diagrams.Backend.OpenSCad.CmdLine
+
+main = defaultMain ex
+
+ex :: Diagram OpenSCad
+ex = scale 20 $ cube <> sphere
